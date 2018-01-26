8fce0933f8bed982a82be05a53fc9cf57a20480f
zizhen.zuo <zizhen.zuo@cachemoment.com>
2018-01-26 23:22:52 +0800

    1.change welcome style 2.bugfix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+var obj = document.getElementById("fame");
+var fameSrc = obj.src;
+if (fameSrc = "welcome.html") {
+	obj.style.background = 'none'
+	}
+	else {
+	obj.style.background = '#fff'
+}
+
+var background = new Image();
+background.src = './assets/img/bg.jpg';
+background.onload = function () {
+    console.log('Background load complete!');
+    var loadbackground = document.getElementById('body_bg');
+    loadbackground.style.backgroundImage = 'url(' + background.src + ')';
+    loadbackground.style.animationName = 'fadein';
+}
+
 $(document).ready(function(){
 	$('.sidebar-space li').click(function(){
 		var tab_id = $(this).attr('data-tab');
@@ -16,5 +34,23 @@ $(function() {
     $("#fame").css("display", "block");
     $(".sub-link .link").parent().removeClass("active");
     $(this).parent().addClass("active")
+		$("#fame").addClass('bg_white');
   });
 });
+
+function time(){
+	hoursShow = document.getElementById('showtime_hours');
+	minutesShow = document.getElementById('showtime_minutes');
+ 	var date_time = new Date()
+	var hours = date_time.getHours();
+	if(hours<10){
+	 	hours="0"+hours;
+	}
+	var minutes = date_time.getMinutes();
+	if(minutes<10){
+	 	minutes="0"+minutes;
+	}
+ 	hoursShow.innerHTML = hours;
+	minutesShow.innerHTML = minutes;
+ 	setTimeout(time,1000);
+}
